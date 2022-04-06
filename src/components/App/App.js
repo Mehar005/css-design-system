bdd994821ab05690b30eef8acde7a9cf741ff58b
Mehar005 <asimali35.aa@gmail.com>
2022-04-06 12:55:54 +0500

    basic css reset added

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="container grid bg-dark">
+      <div className="box">a</div>
+      <div className="box">a</div>
+      <div className="box">a</div>
+      <div className="box">a</div>
+    </div>
+  );
+}
+
+export default App;
